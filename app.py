492591e176bd3dc32adbf427a0cea251ca40ee8c
puridilip <dilippuripuri@gmail.com>
2018-06-23 15:58:20 +0530

    try html fix

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from datetime import datetime
 app = Flask(__name__)
 
-@app.route('/index.html')
+@app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
@@ -13,8 +13,8 @@ def homepage():
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
 
-def render_static(page_name):
-    return render_template('%s.html' % page_name)
+def render_static(index.html):
+    return render_template('%s.html' % index)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
