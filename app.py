0674ee1779703a318d5d0de24e14d8a85b76add8
puridilip <dilippuripuri@gmail.com>
2018-06-23 11:21:34 +0530

    new heroku setup

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-import os
 from flask import Flask
-
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, use_reloader=True)
+
