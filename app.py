4ab40810d1787e7e27d859fe992eec316810f7d4
puridilip <dilippuripuri@gmail.com>
2018-06-24 15:21:02 +0530

    new fix

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def homepage():
 
 @app.route('/index')
 def index():
-    return render_template('%s.html' % index)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
